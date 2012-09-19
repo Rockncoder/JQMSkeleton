ac49c524ec1d2d468760fa04825a4389bdc1fb74
Troy Miles <rockncoder@gmail.com>
2012-09-18 21:18:30 -0700

    Continued cleanup of the skeleton. Shortened some namespaces, moved pages out of apps and into its own file.

--- scripts/app.js
+++ scripts/app.js
@@ -1,80 +1,62 @@
 ﻿
 var RocknCoder = RocknCoder || {};
-RocknCoder.Pages = RocknCoder.Pages || {};
-// put the page events into one string
-RocknCoder.PageEvents = "pagebeforeshow pageshow pagebeforechange pagechange pagebeforehide pagehide";
 
-// the kernel remains unchanged
-RocknCoder.Pages.Kernel = function (event) {
-	var that = this,
-		eventType = event.type,
-		pageName = $(this).attr("data-rnc-jspage");
-	if (RocknCoder && RocknCoder.Pages && pageName && RocknCoder.Pages[pageName] && RocknCoder.Pages[pageName][eventType]) {
-		RocknCoder.Pages[pageName][eventType].call(that);
-	}
-};
-
-// anonymous function which binds to the page's page events
 (function () {
-	$("div[data-rnc-jspage]").on(
-		RocknCoder.PageEvents,
-		RocknCoder.Pages.Kernel
-	);
-}());
+	"use strict";
 
-// anonymous function which binds to the document's pageload event
-(function () {
-	$(document).bind(
-		'pageload',
-		function (event, obj) {
-			console.log("pageload");
-			$("div[data-rnc-jspage]")
-				// to make sure we aren't double hooking events clear them all
-				.off(RocknCoder.PageEvents)
-				// then hook them all  (the newly loaded page is in DOM at this point)
-				.on(RocknCoder.PageEvents, RocknCoder.Pages.Kernel);
+	RocknCoder.Pages = RocknCoder.Pages || {};
+	// put the page events into one string
+	RocknCoder.Events = "pagebeforeshow pageshow pagebeforechange pagechange pagebeforehide pagehide";
+
+	// the kernel remains unchanged
+	RocknCoder.Kernel = function (event) {
+		var that = this,
+			eventType = event.type,
+			pageName = $(this).attr("data-rnc-jspage");
+		if (RocknCoder && RocknCoder.Pages && pageName && RocknCoder.Pages[pageName] && RocknCoder.Pages[pageName][eventType]) {
+			RocknCoder.Pages[pageName][eventType].call(that);
 		}
-	);
-}());
+	};
 
-// size the content area
-RocknCoder.Dimensions = (function () {
-	var get = function () {
-		var isFirstPass = false,
-			isIPhone = (/iphone/gi).test(navigator.appVersion),
-			width = $(window).width(),
-			height = $(window).height() + (isIPhone ?  60 : 0),
-			hHeight = $('header').outerHeight(),
-			fHeight = $('footer').outerHeight();
+	// anonymous function which binds to the page's events
+	(function () {
+		$("div[data-rnc-jspage]").on(
+			RocknCoder.Events,
+			RocknCoder.Kernel
+		);
+	}());
+
+	// anonymous function which binds to the document's pageload event
+	(function () {
+		$(document).bind(
+			'pageload',
+			function (event, obj) {
+				console.log("pageload");
+				$("div[data-rnc-jspage]")
+					// to make sure we aren't double hooking events clear them all
+					.off(RocknCoder.Events)
+					// then hook them all  (the newly loaded page is in DOM at this point)
+					.on(RocknCoder.Events, RocknCoder.Kernel);
+			}
+		);
+	}());
+
+	// size the content area
+	RocknCoder.Dimensions = (function () {
+		var get = function () {
+			var isFirstPass = false,
+				isIPhone = (/iphone/gi).test(navigator.appVersion),
+				width = $(window).width(),
+				height = $(window).height() + (isIPhone ?  60 : 0),
+				hHeight = $('header').outerHeight(),
+				fHeight = $('footer').outerHeight();
+			return {
+				width: width,
+				height: height - hHeight - fHeight
+			};
+		};
 		return {
-			width: width,
-			height: height - hHeight - fHeight
+			get: get
 		};
-	};
-	return {
-		get: get
-	};
+	}());
 }());
-
-RocknCoder.Pages.page1 = (function () {
-	var pageshow = function () {
-		},
-		pagehide = function () {
-		};
-	return {
-		pageshow: pageshow,
-		pagehide: pagehide
-	};
-}());
-
-RocknCoder.Pages.page2 = (function () {
-	var pageshow = function () {
-		},
-		pagehide = function () {
-		};
-	return {
-		pageshow: pageshow,
-		pagehide: pagehide
-	};
-}());
-
---
+++ scripts/pages.js
@@ -0,0 +1,31 @@
+
+var RocknCoder = RocknCoder || {};
+
+(function () {
+	"use strict";
+
+	RocknCoder.Pages = RocknCoder.Pages || {};
+
+	RocknCoder.Pages.page1 = (function () {
+		var pageshow = function () {
+			},
+			pagehide = function () {
+			};
+		return {
+			pageshow: pageshow,
+			pagehide: pagehide
+		};
+	}());
+
+	RocknCoder.Pages.page2 = (function () {
+		var pageshow = function () {
+			},
+			pagehide = function () {
+			};
+		return {
+			pageshow: pageshow,
+			pagehide: pagehide
+		};
+	}());
+}());
+
